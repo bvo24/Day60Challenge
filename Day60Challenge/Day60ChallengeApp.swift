be33708ddc8f382ac72df6327c1c192205d7fd20
bvo24 <brianvo2020@gmail.com>
2024-05-14 19:01:23 -0400

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Brian Vo on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
