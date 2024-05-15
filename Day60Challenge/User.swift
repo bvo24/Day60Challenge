b4eb979a8b29402825a19f9da7c411c2207885b8
bvo24 <brianvo2020@gmail.com>
2024-05-14 21:12:38 -0400

    Very raw but functional

---
+++ Day60Challenge/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Day60Challenge
+//
+//  Created by Brian Vo on 5/14/24.
+//
+
+import Foundation
---
+++ DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Day60Challenge
+//
+//  Created by Brian Vo on 5/14/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
